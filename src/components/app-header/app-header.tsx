5eb4163b95ef4a50ed417fa586f608fd5388b333
R0777 <romka256@yandex.ru>
2021-05-23 14:18:52 +0300

    sprint1-header-styling

--- src/components/app-header/app-header.tsx
+++ src/components/app-header/app-header.tsx
@@ -1,11 +1,9 @@
 import React from 'react';
-import { Logo } from '@ya.praktikum/react-developer-burger-ui-components';
-import { BurgerIcon } from '@ya.praktikum/react-developer-burger-ui-components';
-import { ListIcon } from '@ya.praktikum/react-developer-burger-ui-components';
-import { ProfileIcon } from '@ya.praktikum/react-developer-burger-ui-components';
+import { BurgerIcon, ListIcon, Logo, ProfileIcon } from '@ya.praktikum/react-developer-burger-ui-components';
 import styles from './app-header.module.css';
 
 const AppHeader = () => {
+
   return (
       <header style={{backgroundColor: '#1C1C21'}} className={styles.header}>
         <nav className={styles.header__nav}>
@@ -14,10 +12,12 @@ const AppHeader = () => {
             <li className={styles.nav__list_item}><BurgerIcon type="secondary" />Конструктор</li>
             <li className={styles.nav__list_item}><ListIcon type="secondary" />Лента заказов</li>
           </div>
-          <Logo />
+
             <li className={styles.nav__list_item}><ProfileIcon type="secondary" />Личный кабинет</li>
           </ul>
-          
+          <div className={styles.logo}>
+            <Logo />
+          </div>
         </nav>
       </header>
   );
