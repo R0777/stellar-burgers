eaa7cee5d4a82e0c77c810d9e53c48853a12c75f
R0777 <romka256@yandex.ru>
2021-05-25 00:17:43 +0300

    sprint1-header-styling-mobile

--- src/components/app-header/app-header.tsx
+++ src/components/app-header/app-header.tsx
@@ -4,16 +4,29 @@ import styles from './app-header.module.css';
 
 const AppHeader = () => {
 
-  return (
-      <header style={{backgroundColor: '#1C1C21'}} className={styles.header}>
-        <nav className={styles.header__nav}>
-          <ul className={styles.nav__list}>
-          <div className={styles.nav__list_block}>
-            <li className={styles.nav__list_item}><BurgerIcon type="secondary" />Конструктор</li>
-            <li className={styles.nav__list_item}><ListIcon type="secondary" />Лента заказов</li>
-          </div>
 
-            <li className={styles.nav__list_item}><ProfileIcon type="secondary" />Личный кабинет</li>
+
+  return (
+      <header className={styles.header}>
+        <nav className={styles.header__nav}>
+
+
+        <input id="header__btn_burger" className={styles.header__btn_burger} type="checkbox" />
+        <label htmlFor="header__btn_burger" className={styles.header__btn_menu}>
+        <span></span>
+        </label>
+
+          <ul className={styles.nav__list}>
+          {/* <div className={styles.nav__list_block}> */}
+
+            <li className={styles.nav__list_item}>
+              <div className={styles.nav__icon}><BurgerIcon type="secondary" /></div>Конструктор</li>
+
+
+            <li className={styles.nav__list_item}><div className={styles.nav__icon}><ListIcon type="secondary" /></div>Лента заказов</li>
+          {/* </div> */}
+
+            <li className={styles.nav__list_item}><div className={styles.nav__icon}><ProfileIcon type="secondary" /></div>Личный кабинет</li>
           </ul>
           <div className={styles.logo}>
             <Logo />
