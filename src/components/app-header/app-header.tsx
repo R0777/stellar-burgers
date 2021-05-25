3bfc367166a3c29d420718849abada2fe76c0e97
R0777 <romka256@yandex.ru>
2021-05-26 00:27:01 +0300

    sprint1-header-styling-mobilehead

--- src/components/app-header/app-header.tsx
+++ src/components/app-header/app-header.tsx
@@ -12,21 +12,29 @@ const AppHeader = () => {
 
 
         <input id="header__btn_burger" className={styles.header__btn_burger} type="checkbox" />
+        <input id="header__btn_login" className={styles.header__btn_login} type="checkbox" />
         <label htmlFor="header__btn_burger" className={styles.header__btn_menu}>
         <span></span>
         </label>
 
           <ul className={styles.nav__list}>
-          {/* <div className={styles.nav__list_block}> */}
-
+            <h4 className={styles.nav__list_title}>Меню</h4>
             <li className={styles.nav__list_item}>
               <div className={styles.nav__icon}><BurgerIcon type="secondary" /></div>Конструктор</li>
 
 
             <li className={styles.nav__list_item}><div className={styles.nav__icon}><ListIcon type="secondary" /></div>Лента заказов</li>
-          {/* </div> */}
+          
 
-            <li className={styles.nav__list_item}><div className={styles.nav__icon}><ProfileIcon type="secondary" /></div>Личный кабинет</li>
+            <li className={styles.nav__list_item}><label htmlFor="header__btn_login" className={styles.header__label_login}><div className={styles.nav__icon}><ProfileIcon type="secondary" /></div>Личный кабинет</label>
+
+            </li>
+
+            <ul className={styles.nav__login_list}>
+              <li className={styles.nav__login_list_item}>Вкусности</li>
+              <li className={styles.nav__login_list_item}>Мои заказы</li>
+              <li className={styles.nav__login_list_item}>Выход</li>
+            </ul>
           </ul>
           <div className={styles.logo}>
             <Logo />
