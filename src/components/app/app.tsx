739fccea8fc36e538313fda657a0c4176097d7db
R0777 <romka256@yandex.ru>
2021-05-30 22:57:33 +0300

    new folders

---
+++ src/components/app/app.tsx
@@ -0,0 +1,16 @@
+import React from 'react';
+import AppHeader from '../app-header/app-header';
+import Main from '../main/main';
+import Footer from '../footer/footer';
+
+const App = () => {
+  return (
+    <>
+      <AppHeader />
+      <Main />
+      <Footer />
+    </>
+  )
+}
+
+export default App;
