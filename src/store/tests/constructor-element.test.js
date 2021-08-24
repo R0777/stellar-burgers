9c9312b686a649068ca876ed54bd4002165a825f
R0777 <romka256@yandex.ru>
2021-08-25 00:22:35 +0300

    sprint4-test-elements-full

--- src/store/tests/constructor-element.test.js
+++ src/store/tests/constructor-element.test.js
@@ -1,4 +1,4 @@
-import reducer, { initialState, setBun, version, setTopMiddle, deleteMiddle, setTotal, resetStore, elementCounter, sortConstructor, bredElement, middleElement, elementAmount, total, idBasket } from '../slices/constructor-element';
+import reducer, { initialState, setBun, version, setTopMiddle, deleteMiddle, setTotal, resetStore, elementCounter, sortConstructor } from '../slices/constructor-element';
 
 describe('reducer, actions and selectors', () => {
   it('should return the initial state on first run', () => {
@@ -39,23 +39,57 @@ describe('reducer, actions and selectors', () => {
 
   it('should properly sum elem price + add id to array', () => {
 
-    // const iniState = {
-    //   data: [{price:10}, {price:11}, {price:12}]
-    // }
 
     const payload = {
-      data: [{price:10}, {price:11}, {price:12}],
-      bun: [{price: 13}]
+      data: [{price:10, id: 1}, {price:11, id: 2}, {price:12, id: 3}],
+      bun: [{price: 13, id: 4}]
     };
-    const received = reducer(initialState, setTopMiddle(payload));
-    const expected = { ...initialState, middleElement: [payload] }
+
+    const received = reducer(initialState, setTotal(payload));
+    const expected = { ...initialState, idBasket: [4, 1, 2, 3], total: 46 }
 
     expect(received).toEqual(expected);
   });
 
+  it('should properly reset store', () => {
 
+    const received = reducer(initialState, resetStore());
+    const expected = { ...initialState }
 
+    expect(received).toEqual(expected);
+  });
 
+  it('should properly count elements', () => {
+
+    const iniState = {
+      middleElement: [{name:'a'}, {name: 'ab'}, {name:'abc'}, {name:'abc'}],
+      bredElement: [{name: 'abcd'}]
+    }
+
+    const payload = 'abc';
+
+    const received = reducer(iniState, elementCounter(payload));
+    const expected = { ...iniState, elementAmount: 2 }
+
+    expect(received).toEqual(expected);
+  });
+
+  it('should properly sort elements', () => {
+
+    const iniState = {
+      middleElement: [{ver:1234}, {ver:12345}, {ver:123}]
+    }
+
+    const payload = {
+      item: {ver: 123456},
+      props: {ver: 1234}
+    };
+
+    const received = reducer(iniState, sortConstructor(payload));
+    const expected = { ...iniState, middleElement: [{ver:1234}, {ver:123456}, {ver:12345}, {ver:123}] }
+
+    expect(received).toEqual(expected);
+  });
 
 
 
